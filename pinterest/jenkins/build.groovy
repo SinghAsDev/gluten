e1158dca3fe90840e05521e804738c6ae2ab44ab
Felix Loesing <floesing@pinterest.com>
2024-01-26 11:48:46 -0800

    Add Ubuntu 18 support  (#3)
    ## What changes were proposed in this pull request?
    
    Adding support for Ubuntu 18
    
    ## How was this patch tested?
    
    Ran a query successfully that has been validated on Ubuntu 20 before. No
    performance regression was observed.
    
    <!-- https://github.com/pinternal/gluten/pull/3 -->

--- pinterest/jenkins/build.groovy
+++ pinterest/jenkins/build.groovy
@@ -67,43 +67,44 @@ pipeline {
                         sh "aws s3 cp ${env.WORKSPACE}/out s3://datausers/jenkins/gluten/jars/amd64/ --recursive --exclude '*' --include 'gluten-velox-bundle-spark3*.jar'"
                     }
                 }
-                // stage ('linux/amd64 - Ubuntu 18.04') {
-                //     agent {
-                //         label "amd-worker"
-                //     }
-                //     steps {
-                //         checkout([
-                //             $class: 'GitSCM',
-                //             branches: [[name: params.VELOX_BRANCH]],
-                //             extensions: [
-                //                     [$class: 'RelativeTargetDirectory', relativeTargetDir: 'ep/build-velox/build/velox_ep'],
-                //                     [$class: 'CloneOption', depth: 100, honorRefspec: true, noTags: true, reference: '', shallow: true, timeout: 30]
-                //             ],
-                //             userRemoteConfigs: [[ url: 'org-86262824@github.com:pinternal/velox.git', credentialsId: '145755']]
-                //         ])
-                //         sh "DOCKER_BUILDKIT=1 docker build --no-cache --file pinterest/jenkins/amd64/Ubuntu18/Dockerfile --output out ."
-                //         script {
-                //             def files = findFiles(glob: '**/gluten-velox-bundle-spark3*')
-                //             for (file in files) {
-                //                 def fullPath = file.getPath()  
-                //                 def parentDir = new File(fullPath).parent  
-                //                 def fullName = file.getName()  
-                //                 def fileName = fullName.substring(0, fullName.lastIndexOf('.'))  
-                //                 def fileExt = fullName.substring(fullName.lastIndexOf('.'))  
-                //                 def newFileName = parentDir + '/' + fileName + '_' + env.BUILD_NUMBER + fileExt  
-                //                 sh "mv ${fullPath} ${newFileName}"  
-                //             }
-                //         }
-                //         sh "aws s3 cp ${env.WORKSPACE}/out s3://datausers/jenkins/gluten/jars/amd64/--recursive --exclude '*' --include 'gluten-velox-bundle-spark3*.jar'"
-                //     }
-                // }
+                stage ('linux/amd64 - Ubuntu 18.04') {
+                    agent {
+                        label "amd-worker"
+                    }
+                    steps {
+                        checkout([
+                            $class: 'GitSCM',
+                            branches: [[name: params.VELOX_BRANCH]],
+                            extensions: [
+                                    [$class: 'RelativeTargetDirectory', relativeTargetDir: 'ep/build-velox/build/velox_ep'],
+                                    [$class: 'CloneOption', depth: 100, honorRefspec: true, noTags: true, reference: '', shallow: true, timeout: 30]
+                            ],
+                            userRemoteConfigs: [[ url: 'org-86262824@github.com:pinternal/velox.git', credentialsId: '145755']]
+                        ])
+                        sh "DOCKER_BUILDKIT=1 docker build --no-cache --file pinterest/jenkins/amd64/Ubuntu18/Dockerfile --output out ."
+                        script {
+                            def files = findFiles(glob: '**/gluten-velox-bundle-spark3*')
+                            for (file in files) {
+                                def fullPath = file.getPath()  
+                                def parentDir = new File(fullPath).parent  
+                                def fullName = file.getName()  
+                                def fileName = fullName.substring(0, fullName.lastIndexOf('.'))  
+                                def fileExt = fullName.substring(fullName.lastIndexOf('.'))  
+                                def newFileName = parentDir + '/' + fileName + '_' + env.BUILD_NUMBER + fileExt  
+                                sh "mv ${fullPath} ${newFileName}"  
+                            }
+                        }
+                        sh "aws s3 cp ${env.WORKSPACE}/out s3://datausers/jenkins/gluten/jars/amd64/ --recursive --exclude '*' --include 'gluten-velox-bundle-spark3*.jar'"
+                    }
+                }
             }
         }
     }
     post {
         // Clean after build
         always {
-            cleanWs()
+            // cleanWs()
+            sh "rm -rf ${WORKSPACE}/*"
         }
     }
 }
